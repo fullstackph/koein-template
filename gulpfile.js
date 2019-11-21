2e828452a872c3cb82c249f8301ede1444439d8b
edward-fullstack <edward@fullstack.ph>
2019-11-21 20:02:14 +0800

    Updated packages

--- gulpfile.js
+++ gulpfile.js
@@ -1,48 +1,38 @@
-var gulp   = require('gulp');
-var sass   = require('gulp-sass');
-var rename = require('gulp-rename');
-var babel  = require('gulp-babel');
-var concat = require('gulp-concat');
-var uglify = require('gulp-uglify');
-var browserSync = require('browser-sync').create();
-var autoprefixer = require('gulp-autoprefixer');
-var sourcemaps = require('gulp-sourcemaps');
+const gulp = require("gulp");
+const sass = require("gulp-sass");
+const rename = require("gulp-rename");
+const browserSync = require("browser-sync").create();
+const autoprefixer = require("gulp-autoprefixer");
+const cleanCSS = require("gulp-clean-css");
 
-gulp.task('serve', ['sass', 'transpuglify'], function() {
-	browserSync.init({
-		server: './'
-	});
-	gulp.watch('./stylesheets/**/*.scss', ['sass']);
-	gulp.watch('./javascripts/**/*.js', ['transpuglify']);
-	gulp.watch('./*.html').on('change', browserSync.reload);
-});
+function sassCompile() {
+  return gulp
+    .src("stylesheets/*.scss")
+    .pipe(sass().on("error", sass.logError))
+    .pipe(rename({ basename: "main", suffix: ".min" }))
+    .pipe(gulp.dest("stylesheets"));
+}
 
-gulp.task('sass', function() {
-	return gulp.src('stylesheets/*.scss')
-    .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
-    .pipe(sourcemaps.init())
-    .pipe(autoprefixer({
-			browsers: ['last 2 versions'],
-			cascade: false
-    }))
-    .pipe(sass({outputStyle: 'compressed'}).on('error', sass.logError))
-    .pipe(rename('main.min.css'))
-    .pipe(sourcemaps.write())
-		.pipe(gulp.dest('./stylesheets'))
-		.pipe(browserSync.stream());
-});
+function autoprefixCSS() {
+  return gulp
+    .src("stylesheets/main.min.css")
+    .pipe(autoprefixer({ cascade: false }))
+    .pipe(gulp.dest("stylesheets"));
+}
 
-gulp.task('transpuglify', function () {
-	return gulp.src([
-		'javascripts/main.js',
-		])
-		.pipe(concat('main.min.js'))
-		.pipe(babel({
-			presets: ['babel-preset-env']
-		}))
-		.pipe(uglify())
-		.pipe(gulp.dest('./javascripts'))
-		.pipe(browserSync.stream());
-})
+function minifyCSS() {
+  return gulp
+    .src("stylesheets/main.min.css")
+    .pipe(cleanCSS())
+    .pipe(gulp.dest("stylesheets"));
+}
 
-gulp.task('default', ['serve']);
+function watch() {
+  browserSync.init({ server: "./" });
+  gulp.watch("./stylesheets/**/*.scss", sassCompile);
+}
+
+const build = gulp.series(sassCompile, autoprefixCSS, minifyCSS);
+
+exports.default = watch;
+exports.build = build;
