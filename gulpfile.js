bf3358e6dc26e422905342d5f9e802262c427174
Jed Macapagal <edward@fullstack.ph>
2019-11-21 22:36:57 +0800

    JS Minification

--- gulpfile.js
+++ gulpfile.js
@@ -4,13 +4,16 @@ const rename = require("gulp-rename");
 const browserSync = require("browser-sync").create();
 const autoprefixer = require("gulp-autoprefixer");
 const cleanCSS = require("gulp-clean-css");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
 
 function sassCompile() {
   return gulp
     .src("stylesheets/*.scss")
     .pipe(sass().on("error", sass.logError))
     .pipe(rename({ basename: "main", suffix: ".min" }))
-    .pipe(gulp.dest("stylesheets"));
+    .pipe(gulp.dest("stylesheets"))
+    .pipe(browserSync.stream());
 }
 
 function autoprefixCSS() {
@@ -27,12 +30,25 @@ function minifyCSS() {
     .pipe(gulp.dest("stylesheets"));
 }
 
+function transpileJS() {
+  return gulp
+    .src("javascripts/main.js", { sourcemaps: true })
+    .pipe(babel({ presets: ["@babel/preset-env"] }))
+    .pipe(uglify())
+    .pipe(rename({ basename: "main", suffix: ".min" }))
+    .pipe(gulp.dest("javascripts"));
+}
+
 function watch() {
   browserSync.init({ server: "./" });
   gulp.watch("./stylesheets/**/*.scss", sassCompile);
+  gulp.watch("./*.html").on("change", browserSync.reload);
 }
 
-const build = gulp.series(sassCompile, autoprefixCSS, minifyCSS);
+const build = gulp.parallel(
+  gulp.series(sassCompile, autoprefixCSS, minifyCSS),
+  gulp.series(transpileJS)
+);
 
 exports.default = watch;
 exports.build = build;
